8525f893ce162426026f1da29936e9c95d9001f1
Dalin Kim <dalinkim7@gmail.com>
2019-01-02 10:26:31 -0600

    Using-React-State-in-Diabetes-Self-Management-App

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # diabetes-self-management-app
 
-<b>React-Components-for-Diabetes-Self-Management-App</b><br>
-Web app that reads a global array of diet/exercise/BGL activities and displays them in a Table format using React components. The table have the following columns:<br>
-1. Date (including time in hours and minutes)<br>
-2. Activity Type (possible values: Fasting BGL, BGL After Breakast, BGL After Lunch, BGL After Dinner, Usual Breakfast, Usual Lunch, Usual Dinner, Starbucks, McDonalds, Brisk Walk, Jogging, Relaxed Walk, Swimming)<br>
-3. Value (A string indicating the BGL value in mg/dl or what I ate or how much exercise I did.)<br>
+<b>Using-React-State-in-Diabetes-Self-Management-App</b><br>
+Adding a form to the web app that will allow the user to add a new activity to the displayed list of activities. The form allows the user to enter any text for the "Activity Type" and "Value". The "Date" for the activity is set to the current date.
--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,127 @@
 Date.prototype.today = function () {
-    return ((this.getMonth() < 10) ? "0" : "") + (this.getMonth() + 1) + "/" 
-            + (((this.getDate() + 1) < 10) ? "0" : "") + this.getDate() 
-            + "/" + this.getFullYear();
+    return ((this.getMonth() + 1 < 10) ? "0" : "") + (this.getMonth() + 1) + "/"
+        + ((this.getDate() < 10) ? "0" : "") + this.getDate()
+        + "/" + this.getFullYear();
 }
 Date.prototype.timeNow = function () {
-    return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" 
-            + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" 
-            + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
+    return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":"
+        + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":"
+        + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
 }
 
 const contentNode = document.getElementById('contents');
+const activities = [];
 
-const activities = [
-    {
-        id: 1,
-        date: new Date('2018-12-30'),
-        activity_type: 'Fasting BGL',
-        value: '100'
-    },
-    {
-        id: 2,
-        date: new Date('2018-12-31'),
-        activity_type: 'BGL After Lunch',
-        value: '120'
-    },
-    {
-        id: 3,
-        date: new Date(2019, 0, 1, 11, 11, 11),
-        activity_type: 'Jogging',
-        value: '30 minutes'
-    },
-    {
-        id: 4,
-        date: new Date(),
-        activity_type: 'Usual Dinner',
-        value: 'Ramen'
+// Child does not have access to the parent's method.
+// Need to pass callbacks from the parent to the child, which it can call to achieve specific tasks.
+// createNewActivity from the parent (ActivityList) is passed to the child (ActivityAdd)
+class ActivityAdd extends React.Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-];
 
-class ActivityRow extends React.Component {
+    // prevent default behavior of the form to handle input fields programmatically
+    handleSubmit(e) {
+        e.preventDefault();
+        var form = document.forms.activityAdd;
+        this.props.createNewActivity({
+            date: new Date(),
+            activity_type: form.activity_type.value,
+            value: form.value.value,
+        });
+        // clear the form for the next input
+        form.activity_type.value = '';
+        form.value.value = '';
+    }
+
+    // form with onSubmit instead of onClick
+    // to allow users to press Enter to add a new activity in addition to clicking on the Add button
     render() {
-        const activity = this.props.activity;
-        let date = `${activity.date.today()} ${activity.date.timeNow()}`;
-        
         return (
-            <tr>
-                <td>{activity.id}</td>
-                <td>{date}</td>
-                <td>{activity.activity_type}</td>
-                <td>{activity.value}</td>
-            </tr>
+            <div>
+                <form name="activityAdd" onSubmit={this.handleSubmit}>
+                    <input type="text" name="activity_type" placeholder="Activity Type" />
+                    <input type="text" name="value" placeholder="Value" />
+                    <button>Add</button>
+                </form>
+            </div>
         );
     }
 }
-class ActivityTable extends React.Component {
-    render() {
-        const activityRows = this.props.activities.map(activity =>
-            <ActivityRow key={activity.id} activity={activity} />);
-        return (
-            <table className="bordered-table">
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Date</th>
-                        <th>Activity Type</th>
-                        <th>Value</th>
-                    </tr>
-                </thead>
-                <tbody>{activityRows}</tbody>
-            </table>
-        );
-    }
+
+// stateless component rewritten as a function rather than a class
+const ActivityRow = (props) => (
+    <tr>
+        <td>{props.activity.id}</td>
+        <td>{`${props.activity.date.today()} ${props.activity.date.timeNow()}`}</td>
+        <td>{props.activity.activity_type}</td>
+        <td>{props.activity.value}</td>
+    </tr>
+);
+
+// initial rendering of the IssueTable component now uses the array from the state as its source data
+// stateless component rewritten as a function rather than a class
+const ActivityTable = (props) => {
+    const activityRows = props.activities.map(activity =>
+        <ActivityRow key={activity.id} activity={activity} />);
+    return (
+        <table className="bordered-table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Date</th>
+                    <th>Activity Type</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>{activityRows}</tbody>
+        </table>
+    );
 }
 
 class ActivityList extends React.Component {
+    constructor() {
+        super();
+        this.state = { activities: [] };
+        this.createNewActivity = this.createNewActivity.bind(this);
+    }
+
+    // called immediately after a component is mounted. 
+    // setting state here will trigger re-rendering.
+    componentDidMount() {
+        this.loadData();
+    }
+
+    // uses the global issues list to set the state
+    loadData() {
+        setTimeout(() => {
+            this.setState({ activities: activities });
+        }, 500);
+    }
+
+    // setState tirggers rerendering process for the component
+    // and all descendent components where properties get affected because of the state change.
+    // ActivityTable and ActivityRow will be rerendered, and when they are,
+    // their properties will reflect the new state of the parent ActivityList component automatically.
+    createNewActivity(newActivity) {
+        let newActivities = this.state.activities.slice();
+        newActivity.id = this.state.activities.length + 1;
+        newActivity.date = new Date();
+        newActivities.push(newActivity);
+        this.setState({ activities: newActivities });
+    }
+
+    // passing the data from the state to the IssueTable via properties
+    // replacing the global array with the state data
     render() {
         return (
             <div>
                 <h1>Diabetes Self Managment App</h1>
                 <hr />
-                <ActivityTable activities={activities} />
+                <ActivityTable activities={this.state.activities} />
+                <hr />
+                <ActivityAdd createNewActivity={this.createNewActivity} />
             </div>
         );
     }
